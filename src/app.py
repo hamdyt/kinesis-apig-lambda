d0b0428982379da8105846da3c767f202fdb87ac
hamdyt <eed738@hotmail.com>
2021-11-03 14:48:01 -0500

    Updated app.py
    Adding Lambda function code to app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,28 @@
+import json
+import base64
+
+print('Loading function')
+
+def lambda_handler(event, context):
+                print('------------------------')
+                print(event)
+                #1. Iterate over each record
+                
+                #print("test")
+                
+                try:
+                                for ddbRecord in event['Records']:
+                                                #2. Handle event by type
+                                                
+                                                record = base64.b64decode(ddbRecord["kinesis"]["data"])
+                                                record = json.loads(record.decode('utf-8'))
+                                                
+                                                print('Decoded record: ' + record)
+
+                                print('------------------------')
+                                return "Success!"
+                except Exception as e: 
+                                print(e)
+                                print('------------------------')
+                                return "Error"
 
